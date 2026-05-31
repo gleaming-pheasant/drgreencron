0f0a11610c0a072c131c8029c47e1a93e960c34b
gleaming-pheasant <47186986+gleaming-pheasant@users.noreply.github.com>
2026-05-31 17:23:41 +0100

    Begin process structure walkthrough

--- drgreencrond/src/main.rs
+++ drgreencrond/src/main.rs
@@ -1,3 +1,15 @@
+//! Initialisation steps, first activation or restart:
+//!  - Any tasks to actually schedule (check for .greentimer files)?
+//!     - No: Do nothing except await SIGHUP.
+//!     - Yes: Current schedule set based on today's date (long term task 
+//!     schedule file saved, if just restarted, will need to restart the 
+//!     TimerFds, too. This must be separate one-time logic to the event 
+//!     listener loop)?
+//!         - No, and no failure notification set: Fetch schedule
+//!         - No, and failure notification set: 
+//!         - Yes: Timers 
+
 fn main() {
+    
     println!("Hello, world!");
 }
