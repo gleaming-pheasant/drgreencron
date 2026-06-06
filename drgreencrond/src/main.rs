61a5a66958c72e8d4a2c3ba9884bc11a8327de30
gleaming-pheasant <47186986+gleaming-pheasant@users.noreply.github.com>
2026-06-06 18:02:47 +0100

    make_timer_dirs fn

--- drgreencrond/src/main.rs
+++ drgreencrond/src/main.rs
@@ -8,8 +8,74 @@
 //!         - No, and no failure notification set: Fetch schedule
 //!         - No, and failure notification set: 
 //!         - Yes: Timers 
+use std::collections::HashSet;
+use std::path::PathBuf;
+
+use tracing::warn;
+
+/* Internal decision, may lead to edge-case complaints, in place to prevent 
+ * exhaustive path searching. MAX_DIRS is number of paths on the split, 
+ * MAX_DIR_VAR_LENGTH is MAX_DIRS times a standard default max path length 
+ * setting in linux. */
+const MAX_DIRS: usize = 5;
+const MAX_DIR_VAR_LENGTH: usize = 4096 * MAX_DIRS;
+const DIR_VAR: &'static str = "GREENCRON_DIR";
+
+// Must personally guarantee that these are valid absolute paths. No further 
+// checks are done prior to runtime.
+const DEFAULT_PATHS: [&'static str; 2] = ["/test123", "/test456"];
+
+/// For whenever the system is restarted.
+fn make_timer_dirs() -> HashSet<PathBuf> {
+    let mut dirs: HashSet<PathBuf> = HashSet::with_capacity(
+        MAX_DIRS + DEFAULT_PATHS.len()
+    );
+
+    DEFAULT_PATHS.iter().for_each(|path| { dirs.insert(PathBuf::from(path)); });
+    
+    if let Ok(path_list) = std::env::var(DIR_VAR) {
+        if path_list.len() > MAX_DIR_VAR_LENGTH {
+            warn!("{DIR_VAR} path list too long");
+        } else {
+            let path_split = path_list.split(':');
+            
+            for (ix, path_str) in path_split.enumerate() {
+                
+                if ix == MAX_DIRS {
+                    warn!("too many dirs in {DIR_VAR}");
+                    break;
+                }
+
+                let path = PathBuf::from(path_str);
+
+                // Guard clauses ordered by cost of check for early return.
+                if !path.is_absolute() {
+                    warn!(path = ?path, "received non-absolute dir in {DIR_VAR}");
+                    continue;
+                }
+
+                if !path.is_dir() {
+                    warn!(path = ?path, "dir in {DIR_VAR} is not a directory");
+                    continue;
+                }
+
+                if !path.exists() {
+                    warn!(path = ?path, "dir in {DIR_VAR} doesn't exist");
+                    continue;
+                }
+
+                dirs.insert(path);
+            }
+        }   
+    }
+
+    dirs
+}
 
 fn main() {
-    
-    println!("Hello, world!");
+    let dirs = make_timer_dirs();
+
+    for dir in dirs {
+        println!("{:?}", dir);
+    }
 }
